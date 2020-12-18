f81f5419195b595dee218a3638cac1bf9a4ff989
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2020-12-18 12:58:28 -0600

    update flask ui to show graphs and analysis text

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash
 import logging
 import logging.config
-from config.main_config import RESP19_PATH
+from config.main_config import CITIES19_PATH, RESP19_PATH
 import os
 import pandas as pd
 import visuals as viz
@@ -14,17 +14,41 @@ app.config.from_object('config.flask_config')
 
 logger = logging.getLogger("app")
 
-# loading response data from 2019 for analysis and visualization
+# loading city and response data from 2019 for analysis and visualization
+cities19 = pd.read_csv(CITIES19_PATH)
 resp19 = pd.read_csv(RESP19_PATH)
 
 @app.route("/", methods=['GET','POST'])
 @app.route("/home", methods=['GET','POST'])
 def home():
 
+    # world map showing reporting cities
+    worldmap = viz.create_worldmap(cities19)
+
     # hazard to demographic sankey plot
     hazdemosankey = viz.create_sankey(resp19)
+
+    # bar chart counting cities with vulnerability assessments
+    vulassesplot = viz.create_vulassesbar(resp19)
+
+    # bar chart counting number of times hazards are listed and their
+    # expected probability
+    hazbarplot = viz.create_hazbarplot(resp19)
+
+    # bar charting show adaptataion steps and number of cities undertaking them
+    adapbarplot = viz.create_adaptplot(resp19)
+
+    # bar chart showing factors affecting adaptation
+    adfactbarplot = viz.create_adaptfactorsplot(resp19)
+
     return render_template('home.html',
-                           sankeyplot=hazdemosankey)
+                           sankeyplot=hazdemosankey,
+                           worldplot=worldmap,
+                           vulplot=vulassesplot,
+                           hazplot=hazbarplot,
+                           adapplot=adapbarplot,
+                           adaptfactorsplot=adfactbarplot
+                           )
 
 @app.context_processor
 def override_url_for():
